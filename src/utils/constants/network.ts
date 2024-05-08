6a73415f60b565c189edc44f171b59199b33e478
Min Htet Oo <minhtetoo.dev@gmail.com>
2024-05-08 17:41:04 +0800

    fix: remove maticmum from list of supported networks

--- src/utils/constants/network.ts
+++ src/utils/constants/network.ts
@@ -1,20 +1,19 @@
 export const networks = [
-  "local",
-  "mainnet",
-  "matic",
-  "maticmum",
-  "amoy",
-  "sepolia",
-  "xdc",
-  "xdcapothem",
-  "hederatestnet",
-  "hederamainnet",
-  "stabilitytestnet",
-  "stability",
-] as const;
+    'local',
+    'mainnet',
+    'matic',
+    'amoy',
+    'sepolia',
+    'xdc',
+    'xdcapothem',
+    'hederatestnet',
+    'hederamainnet',
+    'stabilitytestnet',
+    'stability',
+] as const
 
-export type networkName = (typeof networks)[number];
+export type networkName = (typeof networks)[number]
 
-export type networkType = "production" | "test" | "development";
+export type networkType = 'production' | 'test' | 'development'
 
-export type networkCurrency = "ETH" | "MATIC" | "XDC" | "HBAR" | "FREE";
+export type networkCurrency = 'ETH' | 'MATIC' | 'XDC' | 'HBAR' | 'FREE'
