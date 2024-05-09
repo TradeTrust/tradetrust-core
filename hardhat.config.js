d397b369983b988384ba89ccbdc96f6f54d00955
Min Htet Oo <minhtetoo.dev@gmail.com>
2024-05-09 12:48:09 +0800

    ci: trigger ci

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,15 +1,13 @@
-require("@nomiclabs/hardhat-ethers");
+require('@nomiclabs/hardhat-ethers')
 
 module.exports = {
-  defaultNetwork: "hardhat",
-  networks: {
-    hardhat: {
-      chainId: 1337,
-      accounts: {
-        mnemonic: "indicate swing place chair flight used hammer soon photo region volume shuffle"
-      }
-    }
-  },
-  solidity: "0.8.4",
-};
-
+    networks: {
+        hardhat: {
+            chainId: 1337,
+            accounts: {
+                mnemonic:
+                    'indicate swing place chair flight used hammer soon photo region volume shuffle',
+            },
+        },
+    },
+}
