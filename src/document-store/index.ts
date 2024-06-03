101c534759d4b2b1eb75007e5af478d092dc1b1c
Min Htet Oo <33474727+MinHtet-O@users.noreply.github.com>
2024-06-03 18:10:42 +0800

    fix: export methods from document store (#11)

---
+++ src/document-store/index.ts
@@ -0,0 +1,14 @@
+import {
+    type DocumentStore,
+    DocumentStoreFactory,
+    deployAndWait as deployDocumentStore,
+    connect as connectDocumentStore,
+} from '@tradetrust-tt/document-store'
+
+export {
+    DocumentStore,
+    DocumentStoreFactory,
+    deployDocumentStore,
+    connectDocumentStore,
+}
+export * from './getRoleString'
