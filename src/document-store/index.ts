842400e1d17d6bc8905c3302774ddf183a41a255
Min Htet Oo <minhtetoo.dev@gmail.com>
2024-06-12 10:23:26 +0800

    fix: revert deployAndWait method export from document store

--- src/document-store/index.ts
+++ src/document-store/index.ts
@@ -1,14 +1,8 @@
 import {
     type DocumentStore,
     DocumentStoreFactory,
-    deployAndWait as deployDocumentStore,
     connect as connectDocumentStore,
 } from '@tradetrust-tt/document-store'
 
-export {
-    DocumentStore,
-    DocumentStoreFactory,
-    deployDocumentStore,
-    connectDocumentStore,
-}
+export { DocumentStore, DocumentStoreFactory, connectDocumentStore }
 export * from './getRoleString'
