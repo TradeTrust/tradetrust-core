be43aeb75bbc4e20b2c8f470dcd446719b03703a
Min Htet Oo <minhtetoo.dev@gmail.com>
2024-06-21 09:58:38 +0800

    fix: add gasFee calculation logic from tradetrust-cli

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,5 +4,6 @@ export * from './analytics'
 export * from './constants/supportedChains'
 export * from './provider/provider'
 export * from './wallet'
+export * from './gasStation'
 
 export { CONSTANTS }
